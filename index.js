fc271ea66045a5af22576403218ebb52dd6d0ddb
Marko Grešak <marko@gresak.io>
2015-06-03 16:18:11 +0200

    add support for ssl-config.json
    Try to read config file and run https server if read was successful.

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 'use strict';
+var fs = require('fs');
+var path = require('path');
+var https = require('https');
 var express = require('express');
 var bodyParser = require('body-parser');
 var runEvent = require('./run-event');
 
 var port = 9100;
+var conf;
 
+try {
+  // Try to read SSL config file and parse it as JSON.
+  conf = JSON.parse(fs.readFileSync(path.join(__dirname, './ssl-config.json')).toString());
+} catch (e) {
+  // If there was an error while reading, proceed without setting a value to conf.
+  conf = undefined;
+}
 
 // Define app and use JSON body parser.
 var app = express();
@@ -30,5 +41,13 @@ webhooksRouter.post('/', function (req, res) {
 // Use router at /webhooks endpoint.
 app.use('/webhooks', webhooksRouter);
 
+// If config is set and it contains correct keys, start a https server on port.
+if (conf && typeof conf.key === 'string' && typeof conf.cert === 'string') {
+  // Create a https server with passed `conf` an listen on `port`.
+  https.createServer(conf, app).listen(port);
+} else {
+  // Config not passed, assuming https is not required, use regular http.
   app.listen(port);
+}
+
 console.log('webhooksRouter listening on ' + port);
