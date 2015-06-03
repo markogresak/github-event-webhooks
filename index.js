79bdea44450f2d0f27ab56b0f3acfc863c10da52
Marko Grešak <marko@gresak.io>
2015-06-03 16:17:21 +0200

    add index.js script
    Runs a server on port 9100.
    Listens for POST requests at /webhooks.

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var port = 9100;
+
+
+// Define app and use JSON body parser.
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// Set route for webhooks POST request.
+var webhooksRouter = express.webhooksRouter();
+webhooksRouter.post('/', function (req, res) {
+  // Read event from header.
+  var event = req.headers['X-Github-Event'];
+  if (event) {
+    // Run event and end response with 204 (no content).
+    res.status(204).end();
+  } else {
+    // Event is not set, end response with 400 (bad request).
+    res.status(400).end();
+  }
+});
+// Use router at /webhooks endpoint.
+app.use('/webhooks', webhooksRouter);
+
+  app.listen(port);
+console.log('webhooksRouter listening on ' + port);
